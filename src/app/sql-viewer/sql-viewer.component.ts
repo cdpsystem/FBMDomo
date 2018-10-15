40b492c277f2c114ae1852528401d26355f3569a
Cristian Diaz Prado <cdp.officesystem.com>
2018-10-15 16:50:52 +0200

    Cambios SQL viewer
    Añadido styles
    Añadido Código HTML
    Añadido codito TS

--- src/app/sql-viewer/sql-viewer.component.ts
+++ src/app/sql-viewer/sql-viewer.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+
+import { Servidor } from '../models/servidor';
+import { ServidorService } from '../services/servidor.service';
 
 @Component({
   selector: 'sql-viewer',
   templateUrl: './sql-viewer.component.html',
-  styleUrls: ['./sql-viewer.component.css']
+  styleUrls: ['./sql-viewer.component.css'],
+  providers: [ServidorService]
 })
 export class SQLViewerComponent implements OnInit {
 
-  constructor() { }
+	public serverId: string;
+	public serverList: any;
 
-  ngOnInit() {
-  }
+	public lastServerAlias: string;
+	public lastServerDatabase: string;
+
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+	 	private _servidorService: ServidorService
+	){}
+
+	ngOnInit(){
+		this.getServerList();
+
+		this._route.params.subscribe((params: Params) =>{
+			this.lastServerAlias = params.alias;
+			this.lastServerDatabase = params.database;
+			this.serverId = params.id;
+		});
+
+	}
+
+	redirectAddServidor(){
+
+		this._router.navigate(['server/agregar-servidor']);
+	}
+
+  	getServerList(){
+
+	  	this._servidorService.getServerList().subscribe(
+	  		response =>{
+	  			this.serverList = response.server;	  					  		
+	  		},
+	  		error =>{console.log(<any>error);}
+		);
+	  }
 
 }

1b2a0ba41192d8c45c2eb6736270fdeb2b1da764
Cristian Diaz Prado <cdp.officesystem.com>
2018-10-15 16:10:29 +0200

    Cambios en el res.header

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 //CORS
 // Add headers
 app.use(function (req, res, next) {    
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
